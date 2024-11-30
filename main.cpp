f3cb9dbb3f4c0b85aa9c6090fc5ed2ed018571c4
Кузнецова Дария <153662061+trnsfg@users.noreply.github.com>
2024-11-30 21:33:56 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS 
 #include <iostream>
 #include <cstring>
+#include <cmath>
 
 class Position {
-private:
     int x, y;
 
 public:
@@ -16,6 +16,18 @@ public:
 
     void Print() const {
         std::cout << "Position: (" << x << ", " << y << ")" << std::endl;
+    }
+
+    int Distance(const Position& other) const {
+        return static_cast<int>(std::sqrt(std::pow(x - other.x, 2) + std::pow(y - other.y, 2)));
+    }
+
+    void MoveTowards(const Position& target) {
+        if (x < target.x) x++;
+        else if (x > target.x) x--;
+
+        if (y < target.y) y++;
+        else if (y > target.y) y--;
     }
 };
 
@@ -40,8 +52,7 @@ public:
     }
 };
 
-class Sword : public Weapon
-{
+class Sword : public Weapon {
 public:
     Sword(const char* name, int damage)
         : Weapon(name, damage)
@@ -50,8 +61,7 @@ public:
     ~Sword() {}
 };
 
-class Bow : public Weapon
-{
+class Bow : public Weapon {
 public:
     Bow(const char* name, int damage)
         : Weapon(name, damage)
@@ -67,15 +77,12 @@ protected:
     Weapon* weapon;
     Position position;
 
-    void Damaged(int value)
-    {
-        if (hp - value > 0)
-        {
+    void Damaged(int value) {
+        if (hp - value > 0) {
             hp -= value;
             std::cout << this->name << " damaged for " << value << " points." << std::endl;
         }
-        else
-        {
+        else {
             hp = 0;
             std::cout << this->name << " is killed." << std::endl;
         }
@@ -90,8 +97,7 @@ public:
         std::strcpy(this->name, name);
     }
 
-    int GetHP() const
-    {
+    int GetHP() const {
         return hp;
     }
 
@@ -99,8 +105,11 @@ public:
         return name;
     }
 
-    void Attack(Unit& enemy)
-    {
+    Position& GetPosition() {
+        return position;
+    }
+
+    void Attack(Unit& enemy) {
         if (&enemy == this) {
             return;
         }
@@ -115,6 +124,12 @@ public:
         position.Print();
     }
 
+    void MoveTowards(Unit& enemy) {
+        position.MoveTowards(enemy.GetPosition());
+        std::cout << this->name << " moves towards " << enemy.GetName() << " to ";
+        position.Print();
+    }
+
     void PrintPosition() const {
         std::cout << this->name << " ";
         position.Print();
@@ -125,8 +140,8 @@ public:
     }
 };
 
-class Archer : public Unit
-{
+
+class Archer : public Unit {
 private:
     int range;
     int arrows;
@@ -141,8 +156,7 @@ public:
     ~Archer() {}
 };
 
-class Swordman : public Unit
-{
+class Swordman : public Unit {
 private:
     int power;
 public:
@@ -155,75 +169,25 @@ public:
     ~Swordman() {}
 };
 
-class Team
-{
-private:
-    Unit** units;
-    int size;
-
-public:
-    int RandomTarget(Team& units)
-    {
-        return rand() % units.size;
-    }
-    Team() {
-        units = nullptr;
-        size = 0;
-    }
-    Team(Unit** units, int size)
-    {
-        this->units = units;
-        this->size = size;
-        units = nullptr;
-    }
-    Team(const Team& other) = delete;
-    Team& operator = (const Team& other) = delete;
-    void Add(Unit* new_unit)
-    {
-        Unit** temp = units;
-        int new_size = size + 1;
-        units = new Unit * [new_size];
-        for (int i = 0; i < size; i++)
-        {
-            units[i] = temp[i];
-        }
-        units[new_size - 1] = new_unit;
-        size = new_size;
-        delete[] temp;
-    }
-
-    void Attack(Team& enemy_team)
-    {
-        if (!(size > 0 && enemy_team.size > 0))
-        {
-            return;
-        }
-        if (size < enemy_team.size)
-        {
-            int target;
-            for (int i = 0; i < size; i++)
-            {
-                int target = RandomTarget(enemy_team);
-                units[i]->Attack(*enemy_team.units[target]);
-            }
-        }
-    }
-};
-
-int main()
-{
+int main() {
     srand(time(nullptr));
-    Sword Sword1("Sword1", 20);
-    Bow Bow1("Bow1", 15);
 
-    Archer archer1("Bob", 100, 20, 200, &Bow1, Position(5, 5));
-    Swordman swordman1("Rex", 100, 10, &Sword1, Position(0, 0));
+    Sword sword1("Sword1", 20);
+    Bow bow1("Bow1", 15);
+
+    Archer archer1("Bob", 100, 20, 200, &bow1, Position(5, 5));
+    Swordman swordman1("Rex", 100, 10, &sword1, Position(0, 0));
 
     archer1.PrintPosition();
     swordman1.PrintPosition();
 
-    archer1.Move(2, 3);
-    swordman1.Move(-1, 1);
+    int distance = archer1.GetPosition().Distance(swordman1.GetPosition());
+    std::cout << "Distance between аrcher and swordman: " << distance << std::endl;
+
+    while (distance > 0) {
+        swordman1.MoveTowards(archer1);
+        distance = archer1.GetPosition().Distance(swordman1.GetPosition());
+    }
 
     archer1.Attack(swordman1);
     swordman1.Attack(archer1);
